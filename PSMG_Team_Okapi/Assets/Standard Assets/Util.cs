1358782c1de196b4915ec732c6597936905cc708
Caora <biffi92@googlemail.com>
2014-05-12 20:49:40 +0200

    Util.cs hinzugefügt

---
+++ PSMG_Team_Okapi/Assets/Standard Assets/Util.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Util
+{
+
+    public static float GetDistance(Vector3 v1, Vector3 v2) // berechnet horizontalen abstand zwischen zwei vektoren
+    {
+        float a = Mathf.Abs(v1.x - v2.x);
+        float b = Mathf.Abs(v1.z - v2.z);
+
+        return Mathf.Sqrt(a * a + b * b);
+    }
+}
