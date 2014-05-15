817d66e50b1d8440ef52c369bf979821fd4d38e3
philipp <philipp.roehl@stud.uni-regensburg.de>
2014-05-15 21:32:01 +0200

    add step sound, player status and step controller script, add reverb zone to sandbox

---
+++ PSMG_Team_Okapi/Assets/PlayerStatus.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStatus  : MonoBehaviour
+{
+
+    public enum MovementState { none, walk, traverse}
+    public static int playerHealth;
+
+    public MovementState movementState = MovementState.none;
+
+    private float vertAxis;
+    private float horizAxis;
+
+    void Update()
+    {
+
+        vertAxis = Input.GetAxis("Vertical");
+        horizAxis = Input.GetAxis("Horizontal");
+
+        if(vertAxis == 0 && horizAxis == 0)
+        {
+            setState("none");
+        }
+        else if (vertAxis != 0)
+        {
+            setState("walk");
+        }
+        else if (vertAxis == 0 && horizAxis != 0)
+        {
+            setState("traverse");
+        }
+    }
+
+
+    private void setState(string state)
+    {
+        switch(state)
+        {
+            case "none":
+                {
+                    movementState = MovementState.none;
+                    break;
+                }
+            case "walk":
+                {
+                    movementState = MovementState.walk;
+                    break;
+                }
+            case "traverse":
+                {
+                    movementState = MovementState.traverse;
+                    break;
+                }
+        }
+    }
+
+    public string getState()
+    {
+        return movementState.ToString();
+    }
+   
+}
---
+++ PSMG_Team_Okapi/Assets/StepsController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StepsController : MonoBehaviour {
+    public string actualState;
+
+    private PlayerStatus status;
+
+	// Use this for initialization
+	void Start () {
+        status = GetComponent<PlayerStatus>();
+        actualState = status.getState();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        actualState = status.getState();
+        switch(actualState)
+        {
+            case "none":
+                {
+                    audio.Stop();
+                    break;
+                }
+            case "walk":
+                {
+                    if(!audio.isPlaying)
+                    {
+                        audio.Play();
+                    }
+                    break;
+                }
+            case "traverse":
+                {
+                    if (!audio.isPlaying)
+                    {
+                        audio.Play();
+                    }
+                    break;
+                }
+        }
+	
+	}
+}
