707b25cb710b51a1ade79b4dee839a251cc2c6c7
philipp <philipp.roehl@stud.uni-regensburg.de>
2014-06-06 09:22:42 +0200

    add new door model, add door animation script

---
+++ PSMG_Team_Okapi/Assets/Inkubator_State.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inkubator_State : MonoBehaviour {
+
+    public bool isActive;
+
+	// Use this for initialization
+	void Start () {
+        
+        
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (!isActive)
+        {
+            setInactive();
+        }
+        else
+        {
+            setActive();
+        }
+            
+    }
+
+    void setInactive()
+    {
+        int children = transform.childCount;
+            for (int i = 0; i < children; ++i)
+            {
+               
+                GameObject tempObject = transform.GetChild(i).gameObject;
+                if (!tempObject.name.Equals("Sockel")
+                    && !tempObject.name.Equals("Sturz")
+                    && !tempObject.name.Equals("Inkubator_light"))
+                {
+                    
+                    transform.GetChild(i).gameObject.SetActive(false);
+                }
+                if (tempObject.name.Equals("Inkubator_light"))
+                {
+                    tempObject.light.intensity = 1;
+
+                }
+            }     
+        }
+
+    void setActive()
+    {
+        int children = transform.childCount;
+            for (int i = 0; i < children; ++i)
+            {
+               
+                GameObject tempObject = transform.GetChild(i).gameObject;
+                if (!tempObject.name.Equals("Sockel")
+                    && !tempObject.name.Equals("Sturz")
+                    && !tempObject.name.Equals("Inkubator_light"))
+                {
+                    Debug.Log(transform.GetChild(i).gameObject.name);
+                    transform.GetChild(i).gameObject.SetActive (true);
+                }
+                if (tempObject.name.Equals("Inkubator_light"))
+                {
+                    tempObject.light.intensity = 3;
+
+                }
+            }     
+        }
+    }
+
---
+++ PSMG_Team_Okapi/Assets/door_animation.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class door_animation : MonoBehaviour
+{
+
+    public bool isOpen;
+    public bool isLocked;
+    
+
+    public Light[] doorStatusLight;
+
+    public Color neutralLight;
+    public Color lockedLight;
+    public Color unlockedLight;
+
+    private Animator anim;
+    private GameObject player;
+    private Renderer renderer;
+    private bool showStatus = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool(Animator.StringToHash("open"), isOpen);
+        if (!showStatus)
+        {
+            for (int i = 0; i < doorStatusLight.Length; i++)
+            {
+                doorStatusLight[i].color = Color.Lerp(doorStatusLight[i].color, neutralLight, Time.deltaTime * 0.5f);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        showStatus = true;
+        if (other.gameObject == player)
+        {
+            //statusLights();
+            if (!isLocked)
+            {
+                isOpen = true;
+            }
+            else
+            {
+                playLockedSound();
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            statusLights();
+        }
+    }
+
+
+
+    private void statusLights()
+    {
+        for (int i = 0; i < doorStatusLight.Length; i++)
+        {
+            if (isLocked)
+            {
+                doorStatusLight[i].color = Color.Lerp(doorStatusLight[i].color,lockedLight, Time.deltaTime * 5);
+            }
+            else
+            {
+                doorStatusLight[i].color = Color.Lerp(doorStatusLight[i].color, unlockedLight, Time.deltaTime * 5);
+            }
+        }
+    }
+
+    void playLockedSound()
+    {
+        //play Sound
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        isOpen = false;
+        showStatus = false;
+    }
+}
