1940b863b0bea79bb6a9aa1f76410a3bf89395a1
Felix <felix.m.bruckmeier@gmail.com>
2014-06-19 16:41:34 +0200

    refactor: naming is now more consistent; using enum instead of strings where appropriate add: Player_Health class with code previously in Player_Status

---
+++ PSMG_Team_Okapi/Assets/Scripts/Player_Scripts/Player_Health.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Health : MonoBehaviour {
+
+    public delegate void HealthChangeHandler(int newHealth);
+    public event HealthChangeHandler OnHealthChanged = delegate { };
+
+    private int playerHealth;
+
+	// Use this for initialization
+	void Start () {
+        playerHealth = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Debugging
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            this.health = 0;
+        }   
+	}
+
+    public int health
+    {
+        get
+        {
+            return playerHealth;
+        }
+        set
+        {
+            int previousHealth = playerHealth;
+
+            playerHealth = Mathf.Max(0, Mathf.Min(100, value));
+
+            if (playerHealth != previousHealth)
+            {
+                Debug.Log(playerHealth);
+                OnHealthChanged(playerHealth);
+
+                if (playerHealth <= 0)
+                {
+                    GlobalEvents.TriggerOnPlayerDeath();
+                }
+            }
+        }
+    }       
+}
