6aff3d453ddc580c2b0a8875799170597035f554
Felix <felix.m.bruckmeier@gmail.com>
2014-07-01 00:23:11 +0200

    add: AmbientSounds prefab with RandomizeAmbientSounds script

---
+++ PSMG_Team_Okapi/Assets/Scripts/Audio_Scripts/RandomizeAmbientSounds.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomizeAmbientSounds : MonoBehaviour {
+
+    public float minIntervalSec = 30.0f;
+    public float maxIntervalSec = 120.0f;
+
+    public float intervalSec = 0.0f;
+
+    public AudioClip[] audioClips;    
+    	
+	void Start () {
+        // init interval
+        if (intervalSec <= 0)
+        {
+            GenerateRandomInterval();
+        }
+
+        StartCoroutine(PlayRandomSounds());
+	}
+	
+    private void GenerateRandomInterval()
+    {
+        intervalSec = Random.Range(minIntervalSec, maxIntervalSec);
+
+        //intervalSec = 4.0f; // chosen by a fair dice roll
+    }
+
+    IEnumerator PlayRandomSounds()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(intervalSec);
+
+            Vector3 soundLocation = gameObject.transform.position;
+            AudioClip clip = audioClips[Random.Range(0, audioClips.Length)];
+
+            AudioSource.PlayClipAtPoint(clip, soundLocation);
+
+            GenerateRandomInterval();
+        }
+    }
+}
