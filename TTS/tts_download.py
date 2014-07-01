31d6e8d0e19bc62eaa1ea2adaf3fb5d147be531d
Felix Bruckmeier <felix.m.bruckmeier@gmail.com>
2014-07-01 13:45:36 +0200

    add: long files are partitioned by sentences -> split(". ") add: script feedback

--- TTS/tts_download.py
+++ TTS/tts_download.py
@@ -1,14 +1,25 @@
 import requests
 import glob
+import urllib
 
 
 BASE_URL = "http://translate.google.com/translate_tts?tl=en&q=%s"
 
 
-def download_files(texts):
-    responses = [requests.get(BASE_URL % text) for text in texts]
+def download_files(files, texts):
+    contents = []
+    for file_name, text in zip(files, texts):
+        if len(text) > 100:
+            print "text in %s is too long (%s > 100)" % (file_name, len(text))
 
-    return [r.content for r in responses]
+        response = requests.get(BASE_URL % text)
+        if response.status_code != 200:
+            pass
+            #print "text in %s resulted in status code %s" % (file_name, response.status_code)
+
+        contents.append(response.content)
+
+    return contents
 
 
 def save_file(file_name, content):
@@ -27,10 +38,34 @@ def read_texts():
     return files, text_list
 
 
+def partition_long_files(files, texts):
+    new_files, new_texts = [], []
+
+    for file_name, text in zip(files, texts):
+        if len(text) > 100:
+            fs, ts = partition_file(file_name, text)
+            new_files += fs
+            new_texts += ts
+
+        else:
+            new_files.append(file_name)
+            new_texts.append(text)
+
+    return new_files, new_texts
+
+
+def partition_file(file_name, text):
+    parts = text.split(". ")
+    file_names = [file_name[:-4] + "_part" + str(i) + ".txt" for i, _ in enumerate(parts)]
+
+    return file_names, parts
+
+
 if __name__ == '__main__':
     files, texts = read_texts()
+    files, texts = partition_long_files(files, texts)
 
-    contents = download_files(texts)
+    contents = download_files(files, texts)
 
-    for item in zip(files, contents):
-        save_file(item[0][:-4] + ".mp3", item[1])
+    for file_name, content in zip(files, contents):
+        save_file(file_name[:-4] + ".mp3", content)
