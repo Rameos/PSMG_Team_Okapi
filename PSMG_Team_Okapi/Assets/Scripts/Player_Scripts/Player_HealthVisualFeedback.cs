812826fd55c2f13d52afc965080fffd2be967f0f
Felix Bruckmeier <felix.m.bruckmeier@gmail.com>
2014-07-07 15:34:19 +0200

    add: HealthVisualFeedback script and functionality in sandbox

---
+++ PSMG_Team_Okapi/Assets/Scripts/Player_Scripts/Player_HealthVisualFeedback.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_HealthVisualFeedback : MonoBehaviour {
+
+    public float alphaStage1 = 0.20f;
+    public float alphaStage2 = 0.35f;
+    public float alphaStage3 = 0.55f;
+
+    public int healthThresholdStage1 = 90;
+    public int healthThresholdStage2 = 50;
+    public int healthThresholdStage3 = 15;
+
+	// Use this for initialization
+	void Start () {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Player_Health health = player.GetComponent<Player_Health>();
+        health.OnHealthChanged += OnHealthChanged;
+	}
+
+    void Awake()
+    {
+        guiTexture.pixelInset = new Rect(300f, 100f, Screen.width-600, Screen.height-200);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnHealthChanged(int newHealth, int oldHealth)
+    {
+        Color currentColor = guiTexture.color;
+
+        if (newHealth <= healthThresholdStage3)
+        {
+            currentColor.a = alphaStage3;
+        } 
+        else if (newHealth <= healthThresholdStage2)
+        {
+            currentColor.a = alphaStage2;
+        }
+        else if (newHealth <= healthThresholdStage1)
+        {
+            currentColor.a = alphaStage1;
+        }
+
+        guiTexture.color = currentColor;
+    }
+}
