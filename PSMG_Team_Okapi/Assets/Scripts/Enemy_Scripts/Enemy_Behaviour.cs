2092267095bde33c8d0341900ea370a4ff1a28fc
Felix Bruckmeier <felix.m.bruckmeier@gmail.com>
2014-07-08 12:11:03 +0200

    add: freeze controller scripts change: global scripts prefab add: meta files for sounds

--- PSMG_Team_Okapi/Assets/Scripts/Enemy_Scripts/Enemy_Behaviour.cs
+++ PSMG_Team_Okapi/Assets/Scripts/Enemy_Scripts/Enemy_Behaviour.cs
@@ -193,7 +193,7 @@ public class Enemy_Behaviour : MonoBehaviour
     {
         yield return new WaitForSeconds(freezetime);
 
-        enemyState.Alert();
+        //enemyState.Alert();
     }
 
     public void ResetCurrentpoint()
