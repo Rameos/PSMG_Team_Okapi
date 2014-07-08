3f441cec422776a87c469693292d673f8cf8b164
philipp <philipp.roehl@stud.uni-regensburg.de>
2014-07-08 13:20:23 +0200

    add script enemy_animations

---
+++ PSMG_Team_Okapi/Assets/Scripts/Enemy_Scripts/Enemy_Animations.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Animations : MonoBehaviour
+{
+
+    private Animator anim;
+
+    private Enemy_States enemyState;
+    private int idle = 0;
+    private int alert = 1;
+    private int angry = 2;
+    private int frozen = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        enemyState = gameObject.GetComponent<Enemy_States>();
+        enemyState.OnIdle += OnIdle;
+        enemyState.OnAlert += OnAlert;
+        enemyState.OnAngry += OnAngry;
+        enemyState.OnFreeze += OnFreeze;
+    }
+
+    private void OnIdle()
+    {
+        setAnimVar(idle);
+    }
+
+    private void OnAlert()
+    {
+        setAnimVar(alert);
+    }
+
+    private void OnAngry()
+    {
+        setAnimVar(angry);
+    }
+
+    private void OnFreeze()
+    {
+        setAnimVar(frozen);
+    }
+
+
+    private void setAnimVar(int val)
+    {
+        anim.SetInteger(Animator.StringToHash("state"), val);
+    }
+}
+   
+   
