dbb477e3fe113af5baa6882557e643193e9e11c6
philipp <philipp.roehl@stud.uni-regensburg.de>
2014-07-30 14:28:26 +0200

    some changes to serverschrank, add terminal and terminal script

---
+++ PSMG_Team_Okapi/Assets/Terminal_Behaviour.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Terminal_Behaviour : MonoBehaviour {
+    public Material onMaterial;
+    public Material offMaterial;
+    
+    private bool alreadyPlayed;
+    private bool canPlay;
+    private GameObject player;
+    private Material actMaterial;
+
+
+	// Use this for initialization
+	void Start () {
+        alreadyPlayed = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+        actMaterial = gameObject.renderer.material;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(!audio.isPlaying)
+        {
+            actMaterial = offMaterial;
+        }
+        else
+        {
+            actMaterial = onMaterial;
+        }
+        gameObject.renderer.material = actMaterial;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (canPlay)
+            {
+                playbackAudio();
+            }
+        }
+
+	}
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject == player)
+        {
+            canPlay = true;
+            if (!alreadyPlayed)
+            {
+                playbackAudio();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        canPlay = false;
+    }
+
+    private void visualPlaybackFeedback()
+    {
+        actMaterial = onMaterial;
+        gameObject.renderer.material = actMaterial;
+    }
+
+    private void playbackAudio()
+    {
+        if (!audio.isPlaying)
+        {
+            audio.Play();
+            visualPlaybackFeedback();
+            alreadyPlayed = true;
+        }
+    }
+}
