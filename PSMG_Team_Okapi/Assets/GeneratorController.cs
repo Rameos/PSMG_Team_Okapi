c1caf6f923f6e092c207ff81d42ee7c932e2b2b4
philipp <philipp.roehl@stud.uni-regensburg.de>
2014-08-13 17:47:24 +0200

    add generator room to level 2

---
+++ PSMG_Team_Okapi/Assets/GeneratorController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneratorController : MonoBehaviour {
+
+    public GameObject associatedSwitch;
+    public bool isOn;
+    public float speed;
+    public float lightBrightness;
+    public float lightReach;
+    public float flareBrightness;
+    public float particleSpeed;
+
+    //private Animation animation;
+    private ParticleSystem particleSys;
+    private GameObject light;
+    private LensFlare flare;
+    
+
+
+    // Use this for initialization
+    void Start()
+    {
+        animation["Default Take"].speed = 0;
+        animation.wrapMode = WrapMode.Loop;
+
+        Transform mychildtransform = transform.FindChild("Point light");
+        light = mychildtransform.gameObject;
+
+        flare = light.GetComponent<LensFlare>();
+
+        particleSys = transform.FindChild("Particle System").gameObject.particleSystem;
+       
+        
+        if (associatedSwitch != null)
+        {
+            associatedSwitch.GetComponent<Generator_Switch_Controller>().OnActivateGeneratorSwitch += OnActivateSwitch;
+        }
+    }
+
+    private void OnActivateSwitch(GameObject generator)
+    {
+        Debug.Log("HalloWelt");
+       
+            isOn = !isOn;
+            Debug.Log("Generator: " + isOn);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOn)
+        {
+            animation["Default Take"].speed = Mathf.Lerp(animation["Default Take"].speed, 1.0f, Time.deltaTime * speed);
+            Debug.Log(animation["Default Take"].speed);
+
+            light.light.intensity = Mathf.Lerp(light.light.intensity, lightBrightness, Time.deltaTime * speed);
+            light.light.range = Mathf.Lerp(light.light.range, lightReach, Time.deltaTime * speed);
+            flare.brightness = Mathf.Lerp(flare.brightness, flareBrightness, Time.deltaTime * speed);
+            if(!particleSys.isPlaying)
+            {
+                particleSys.Play();
+            }
+            particleSys.playbackSpeed = Mathf.Lerp(particleSys.playbackSpeed, particleSpeed, Time.deltaTime * speed);
+        }else
+        {
+            animation["Default Take"].speed = 0;
+            light.light.intensity = 0;
+            light.light.range = 0;
+            flare.brightness = 0;
+            particleSys.playbackSpeed = 0;
+            
+        }
+    }
+}
---
+++ PSMG_Team_Okapi/Assets/Scripts/Environement_Scripts/Generator_Switch_Controller.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator_Switch_Controller : MonoBehaviour
+{
+
+    public GameObject assoziatedGenerator;
+    public Material lightOnMat;
+    public Material lightOffMat;
+    public bool isOn;
+
+    private bool canPress = false;
+    
+    private GameObject player;
+    private Animator anim;
+
+    public delegate void GeneratorSwitchEventHandler(GameObject generator);
+    public event GeneratorSwitchEventHandler OnActivateGeneratorSwitch;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = gameObject.GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (canPress)
+            {
+                isOn = !isOn;
+                anim.SetBool(Animator.StringToHash("isOn"), isOn);
+                foreach (Transform child in gameObject.transform)
+                {
+                    for (int i = 0; i < child.gameObject.renderer.materials.Length; i++)
+                    {
+                        Material[] mats = child.gameObject.renderer.materials;
+                        if (child.gameObject.renderer.materials[i].name == "Light_Switch_Button_Light_Off (Instance)")
+                        {
+                            mats[i] = lightOnMat;
+                        }
+                        else if (child.gameObject.renderer.materials[i].name == "Light_Switch_Button_Light_On (Instance)" )
+                        {
+                            mats[i] = lightOffMat;
+                        }
+                        child.gameObject.renderer.materials = mats;
+
+                    }
+                }
+
+              
+                if(OnActivateGeneratorSwitch != null)
+                {
+                    // trigger Event
+                    Debug.Log("Trigger");
+
+                    OnActivateGeneratorSwitch(assoziatedGenerator);
+                }
+                audio.Play();
+            }
+        }
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter");
+        if (other.gameObject == player)
+        {
+            canPress = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        canPress = false;
+    }
+}
+
