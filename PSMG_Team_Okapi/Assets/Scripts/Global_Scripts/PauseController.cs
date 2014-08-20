325e26b101e9b89fe4df0167ae29f1ad50441439
Felix Bruckmeier <felix.m.bruckmeier@gmail.com>
2014-08-20 13:39:33 +0200

    add: PauseController script

---
+++ PSMG_Team_Okapi/Assets/Scripts/Global_Scripts/PauseController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseController : MonoBehaviour {
+        
+    public bool debug = false;
+
+    private bool isPaused = false;
+    private float previousTimeScale = 1;
+
+    private GameObject player;
+    private GameObject mainCamera;
+
+    private static PauseController instance;
+
+    public static PauseController Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                instance = (PauseController)FindObjectOfType(typeof(PauseController));
+                {
+                    if (!instance)
+                    {
+                        Debug.LogError("Couldn't find ScreenFader");
+                    }
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");        
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown("p")) {
+            if(!isPaused) {
+                Pause();
+            } else {
+                UnPause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+        previousTimeScale = Time.timeScale;
+        Time.timeScale = 0;
+        Debug.Log(player.GetComponent("MouseLook"));
+        player.GetComponent<MouseLook>().enabled = false;
+        Debug.Log(mainCamera.GetComponent<MouseLook>());
+        mainCamera.GetComponent<MouseLook>().enabled = false;
+    }
+
+    public void UnPause()
+    {
+        isPaused = false;
+        Time.timeScale = previousTimeScale;
+        player.GetComponent<MouseLook>().enabled = true;
+        mainCamera.GetComponent<MouseLook>().enabled = true;
+    }
+}
