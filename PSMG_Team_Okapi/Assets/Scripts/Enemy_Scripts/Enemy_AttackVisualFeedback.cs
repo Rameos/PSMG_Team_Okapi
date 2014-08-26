749065c07b53abe0fc6df2ac49ed3113275ba35e
Felix Bruckmeier <felix.m.bruckmeier@gmail.com>
2014-08-26 13:49:25 +0200

    add: Enemy_AttackVisualFeedback script

---
+++ PSMG_Team_Okapi/Assets/Scripts/Enemy_Scripts/Enemy_AttackVisualFeedback.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_AttackVisualFeedback : MonoBehaviour {
+
+    public ParticleSystem[] particleSystems;
+
+    private Enemy_PlayerAttacking playerAttacking;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Enemy_PlayerAttacking>().OnAttackingPlayer += OnAttackingPlayer;
+	}
+
+    private void OnAttackingPlayer()
+    {
+        foreach (ParticleSystem parSys in particleSystems) {
+            parSys.Play();
+        }
+    }
+}
