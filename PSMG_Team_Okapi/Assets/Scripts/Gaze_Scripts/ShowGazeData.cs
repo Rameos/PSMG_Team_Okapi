7c2d2e4dd644d8a594ee0bf44b911f0d6fcd4562
Felix Bruckmeier <felix.m.bruckmeier@gmail.com>
2014-09-10 13:35:03 +0200

    add: gazeCursor toggle (F9 key)

--- PSMG_Team_Okapi/Assets/Scripts/Gaze_Scripts/ShowGazeData.cs
+++ PSMG_Team_Okapi/Assets/Scripts/Gaze_Scripts/ShowGazeData.cs
@@ -24,6 +24,11 @@ public class ShowGazeData : MonoBehaviour {
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.F9))
+        {
+            showGazeCursor = !showGazeCursor;
+        }
+
         if (showRayCast)
         {
             //Debug.DrawLine(Camera.main.transform.position, new Vector3(Screen.width / 2, Screen.height / 2, 0), Color.yellow);
