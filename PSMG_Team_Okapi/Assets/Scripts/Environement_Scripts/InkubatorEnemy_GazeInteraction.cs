6d33f17682c6ef52e9a06b62556c4fbfe2e6b048
Felix Bruckmeier <felix.m.bruckmeier@gmail.com>
2014-09-10 11:28:09 +0200

    add: InkubatorEnemy scripts

---
+++ PSMG_Team_Okapi/Assets/Scripts/Environement_Scripts/InkubatorEnemy_GazeInteraction.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using iViewX;
+
+public class InkubatorEnemy_GazeInteraction : MonoBehaviourWithGazeComponent {
+
+    public bool debug = false;
+
+    public delegate void InkubatorEnemyGazeHandler();
+    public event InkubatorEnemyGazeHandler OnEnemyGazeEntered = delegate { };
+    public event InkubatorEnemyGazeHandler OnEnemyGazeStay = delegate { };
+    public event InkubatorEnemyGazeHandler OnEnemyGazeExited = delegate { };
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void OnGazeEnter(RaycastHit hit)
+    {
+        if (debug)
+        {
+            Debug.Log("GazeEnter");
+        }
+    }
+
+    public override void OnGazeStay(RaycastHit hit)
+    {
+
+    }
+
+    public override void OnGazeExit()
+    {
+        if (debug)
+        {
+            Debug.Log("GazeExit");
+        }
+    }
+}
