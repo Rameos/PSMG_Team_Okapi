b61d74b59f0f818008accef432a698c7f1a8270a
philipp <philipp.roehl@stud.uni-regensburg.de>
2014-09-10 11:47:26 +0200

    add TTs files to assets, add tts trigger prefab

---
+++ PSMG_Team_Okapi/Assets/TriggerAudioSourceController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerAudioSourceController : MonoBehaviour {
+
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject == player)
+        {
+            if(!audio.isPlaying)
+            {
+                audio.Play();
+            }
+        }
+    }
+}
